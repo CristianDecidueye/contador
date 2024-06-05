4e7a29ff4b46b537135a00917c2ab1bf1617c12b
Cristian Diaz <diazcristian0210@gmail.com>
2024-06-04 21:28:19 -0300

    agregamos la funcion sumar

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,10 +1,20 @@
 
 const Contador = () => {
+    //Aqui agrego la logica del componente
+    let numero = 10;
+
+    const sumar = ()=>{
+      numero +1;
+      console.log(numero);  
+    }
+
     return (
+        // Aqui puede agregar un poco de logica
         <section className="text-center">
-            <h2></h2>
-            <p>1</p>
-            <button className="btn btn-primary me-2">+1</button>
+            {/*Aqui va la zona del maquetado */}
+            <h2>Contador</h2>
+            <p>{numero}</p>
+            <button className="btn btn-primary me-2" onClick={sumar}>+1</button>
             <button className="btn btn-danger">-1</button>
         </section>
     );
