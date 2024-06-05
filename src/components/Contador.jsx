ffe8f800aef375ff560f26333423204e7ca95b4d
Cristian Diaz <diazcristian0210@gmail.com>
2024-06-04 21:41:13 -0300

    agregamos una version mas corta para invocar a la funcion sumar

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -4,12 +4,11 @@ const Contador = () => {
     //Aqui agrego la logica del componente
    // let numero = 10;
 //Aqui creo el state
-const [numero, setNumero] = useState(10);
-    const sumar = ()=>{
+ const [numero, setNumero] = useState(10);
+ //   const sumar = ()=>{
      // numero = numero +1;
-     setNumero(numero + 1)
-      console.log(numero);  
-    }
+ //    setNumero(numero + 1)  
+ //   }
 
     return (
         // Aqui puede agregar un poco de logica
@@ -17,7 +16,7 @@ const [numero, setNumero] = useState(10);
             {/*Aqui va la zona del maquetado */}
             <h2>Contador</h2>
             <p>{numero}</p>
-            <button className="btn btn-primary me-2" onClick={sumar}>+1</button>
+            <button className="btn btn-primary me-2" onClick={()=> setNumero(numero + 1)}>+1</button>
             <button className="btn btn-danger">-1</button>
         </section>
     );
